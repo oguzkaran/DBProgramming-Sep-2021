5489ad67b6197bf2b870be0171732cfa9f79fecc
Oğuz Karan <oguzkaran@csystem.org>
2021-12-10 21:28:18 +0300

    Update

--- src/mssqlsample.sql
+++ src/mssqlsample.sql
@@ -19,76 +19,75 @@
 		- Tüm kortların bilgilerini detaylı olarak getiren sorguyu yazınız
 		- Tüm kortların bilgilerini detaylı olarak Türkçe karşılıkları ile getiren sorguyu yazınız	
 ----------------------------------------------------------------------------------------------------------------------*/
-create function get_full_text(@s1 nvarchar(max), @s2 nvarchar(max), @s3 nvarchar(max))
-returns nvarchar(max)
-as
-begin
-	declare @full_text nvarchar(max) = ''
-
-	if @s1 is not null
-		set @full_text = @s1
-
-	if @s2 is not null
-	begin
-		if @full_text <> ''
-			set @full_text = @full_text + ' '
-		
-		set @full_text = @full_text + @s2
-	end
-
-	if @s3 is not null
-	begin
-		if @full_text <> ''
-			set @full_text = @full_text + ' '
-		
-		set @full_text = @full_text + @s3
-	end		
-
-
-	return @full_text
-end
+create database tennisclubappdb
 
 go
 
-
-create function get_marital_status_text_tr(@status int)
-returns nvarchar(8)
-as
-begin
-	return  case @status
-		when 0 then 'evli'
-		when 1 then 'bekar'
-		when 2 then 'boşanmış'
-		else 'belirsiz'
-		end	
-end
-
-
+use tennisclubappdb
 
 go
 
-create table people (
-	person_id int identity(1, 1) primary key,
-	citizen_id char(11) not null,
-	first_name nvarchar(100) not null,
-	middle_name nvarchar(100),
-	family_name nvarchar(100) not null,
-	marital_status int not null
+create table court_status (
+	court_status_id int primary key identity(1, 1),
+	description nvarchar(32) not null
 )
 
 go
 
-create function get_person_by_id(@person_id int)
-returns table
-as
-return (
-	select citizen_id, 
-	dbo.get_full_text(first_name, middle_name, family_name) as fullname,
-	dbo.get_marital_status_text_tr(marital_status) as marital_status
-	from people where person_id = @person_id
+insert into court_status (description) 
+values
+('Reserved'),
+('Available'),
+('Not Available')
+
+go
+
+create table court_types (
+	court_type_id int primary key identity(1, 1),
+	description nvarchar(32) not null
 )
 
 go
 
+insert into court_types (description) 
+values
+('Open'),
+('Closed'),
+('OpenOrClosed')
 
-select * from get_person_by_id(1015)
+go
+
+create table courts (
+	court_id int primary key identity(1, 1),
+	name nvarchar(100) not null,
+	court_status_id int references court_status(court_status_id),
+	court_type_id int references court_types(court_type_id),
+)
+
+go
+
+-- Tüm kortların bilgilerini detaylı olarak getiren sorguyu yazınız
+
+select 
+	c.name, cs.description, ct.description
+from 
+court_status cs inner join courts c on cs.court_status_id = c.court_status_id
+inner join court_types ct on ct.court_type_id = c.court_type_id
+
+-- Tüm kortların bilgilerini detaylı olarak Türkçe karşılıkları ile getiren sorguyu yazınız	
+
+select 
+c.name,
+case c.court_type_id
+	when 1 then 'Açık'
+	when 2 then 'Kapalı'
+	else 'Açık veya Kapalı'
+end,
+case c.court_status_id
+	when 1 then 'Rezerve Edilmiş'
+	when 2 then 'Müsait'
+	else 'Müsait değil'
+end
+from courts c
+
+
