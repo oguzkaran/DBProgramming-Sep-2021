90c5d03716a6be11b5125f864d00c459bd7c2fe3
Oğuz Karan <oguzkaran@csystem.org>
2022-01-14 21:29:04 +0300

    Update

--- src/mssqlsample.sql
+++ src/mssqlsample.sql
@@ -1,78 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Aşağıda belirtilen people, people_younger ve people_older isimli tablolara göre soruları 
-	yanıtlayınız
+	Sınıf Çalışması: Aşağıda belirtilen tablolara göre soruları yanıtlayınız:
 
-	- people
-		citizen_id p.k.
+	- sensors
+		sensor_id
 		name
-		surname
-		birth_date
-		address
+		host
+		port
+	- sensor_data
+		sensor_data_id
+		sensor_id
+		data real
+		read_date_time
+		
+	- Parametresi ile aldığı name, host, port ve data bilgilerine göre sensor'ü ve sensor'ün verisini ekleyen procedure'ü
+	yazınız
 
-	- people_younger
-		citizen_id p.k.
-		name
-		surname
-		birth_date
-		address
-	- people_older
-		citizen_id p.k.
-		name
-		surname
-		birth_date
-		address
-	Sorular:
-		- Paremetresi ile aldığı person bilgilerine göre, yine parametresi iki tane yaş bilgisinden harektle birinci 
-		yaş bilgisinden küçük olan kişileri people_younger, birinci ve ikinci yaş bilgisi arasındaki kişileri people
-		tablosuna, ikinci yaş bilgisinden büyük olan kişileri ise people_older tablosuna ekleyen stored procedure'ü 
-		yazınız. 	
+	- Paremetresi ile aldığı sensor_id, data ve okuma zamanı bilgisine göre sensor verisini ekleyen procedure'ü yazınız
 ----------------------------------------------------------------------------------------------------------------------*/
 
-create table people (
-	citizen_id char(11) primary key,
-	name nvarchar(100) not null,
-	surname nvarchar(100) not null,
-	birth_date date not null,
-	address nvarchar(max) not null
-)
 
-go
+insert into devices (ip_address, name) values ('192.168.1.223', 'test')
 
-create table people_younger (
-	citizen_id char(11) primary key,
-	name nvarchar(100) not null,
-	surname nvarchar(100) not null,
-	birth_date date not null,
-	address nvarchar(max) not null
-)
+select @@IDENTITY
 
-go
-
-create table people_older (
-	citizen_id char(11) primary key,
-	name nvarchar(100) not null,
-	surname nvarchar(100) not null,
-	birth_date date not null,
-	address nvarchar(max) not null
-)
-
-go
-
-create procedure sp_insert_person(@citizen_id char(11), @name nvarchar(100), @surname nvarchar(100), @birth_date date, @address nvarchar(max), @min_age real, @max_age real)
-as
-begin
-	declare @age real = DATEDIFF(DAY, @birth_date, GETDATE()) / 365.
-
-	if @age < @min_age
-		insert into people_younger (citizen_id, name, surname, birth_date, address) values (@citizen_id, @name, @surname, @birth_date, @address)
-	else if @age < @max_age
-		insert into people (citizen_id, name, surname, birth_date, address) values (@citizen_id, @name, @surname, @birth_date, @address)
-	else
-		insert into people_older (citizen_id, name, surname, birth_date, address) values (@citizen_id, @name, @surname, @birth_date, @address)
-end
-
-go
-
-exec sp_insert_person '12345678912', 'Oğuz', 'Karan', '1976-09-10', 'Riva', 18, 65
-
-exec sp_insert_person '12345678912', 'Oğuz', 'Karan', '1976-09-10', 'Riva', 18, 65
+select * from devices
