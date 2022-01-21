91339a4151ca4c7dd57666f24c72b7ded586e318
Oğuz Karan <oguzkaran@csystem.org>
2022-01-21 21:06:22 +0300

    Update

--- src/mssqlsample.sql
+++ src/mssqlsample.sql
@@ -1,65 +1,86 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Aşağıda belirtilen tablolara göre soruları yanıtlayınız:
+	Sınıf Çalışması: Aşağıdaki tabloları schooldb veritabanında oluşturunuz
+	- students
+		- student_id
+		- citizen_number
+		- first_name
+		- middle_name
+		- last_name
+		- birth_date
+		- address
+	 - lectures
+		- lecture_code char(7) primary key
+		- name
+		- credits
+	- grades
+		- grade_id
+		- description
+		- value
+	- enrolls
+		- enroll_id
+		- student_id
+		- lecture_code
+		- grade_id
 
-	- sensors
-		sensor_id
-		name
-		host
-		port
-	- sensor_data
-		sensor_data_id
-		sensor_id
-		data
-		read_date_time
-		
-	- Parametresi ile aldığı name, host, port ve data bilgilerine göre sensor'ü ve sensor'ün verisini ekleyen procedure'ü
-	yazınız
-
-	- Paremetresi ile aldığı sensor_id, data ve okuma zamanı bilgisine göre sensor verisini ekleyen procedure'ü yazınız
+	AA	-> 4.0
+	BA	-> 3.5
+	BB	-> 3.0
+	CB	-> 2.5
+	CC	-> 2.0
+	DC	-> 1.5
+	DD	-> 1.0
+	FD	-> 0.5
+	FF	-> 0.0
+	NA	-> -1
+	P	-> -1
+	Bu bilgilere göre 
+	- MAT 101 dersi için öğrencilerin sayını notlara göre gruplayarak getiren sorgu
+	- Kredi toplamları 100'den büyük olan öğrencilerin bilgilerini getiren sorgu
+	- Dersleri alan öğrencilerin sayısını derse göre gruplayan sorgu
+	- Dersi 3'den fazla kez alan öğrencilerin kayıt olduğu derleri gruplayan sorgu
+	- Dersi 3'den fazla kez alan öğrencinin kayıt olduğu derslerin not ortalamasını getiren sorgu
 ----------------------------------------------------------------------------------------------------------------------*/
-create table sensors (
-	sensor_id int primary key identity(1, 1),
+
+create database shoppingdb
+
+go
+
+use shoppingdb
+
+go
+
+create table categories (
+	category_id int primary key identity(1, 1),
+	description nvarchar(50) not null
+)
+
+go
+
+insert into categories (description) values ('Electronics'), ('Wear'), ('Auto')
+
+
+go
+
+create table products (
+	product_id int primary key identity(1, 1),
+	category_id int foreign key references categories(category_id),
+	code varchar(100) unique not null,
 	name nvarchar(100) not null,
-	host char(15) not null,
-	port int check(1023 < port and port < 65536) not null
+	stock int check(stock >= 0) not null,
+	cost money not null,
+	price money not null
 )
 
 go
 
-create table sensor_data (
-	sensor_data_id bigint primary key identity(1, 1),
-	sensor_id int foreign key references sensors(sensor_id) not null,
-	data real not null,
-	read_date_time datetime
-)
+-- Stokta bulunan ürünleri ketegorilerine göre gruplanlayarak kar-zarar durumunu veren sorgu
+select SUM(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
+where p.stock > 0 group by c.category_id, c.description
 
-go
+-- Stokta bulunan ürünleri kategorilerine göre gruplayarak en çok kar getiren ürünlerin fiyatlarını veren sorgu
+select MAX(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
+where p.stock > 0 group by c.category_id, c.description having MAX(p.stock * (p.price - p.cost)) > 0
 
-create procedure sp_insert_sensor_with_port(@name nvarchar(100), @host char(15), @port int, @data real)
-as
-begin
-	-- ileride transaction eklenecek
-	insert into sensors (name, host, port) values (@name, @host, @port)
-	declare @sensor_id int = @@IDENTITY
-
-	insert into sensor_data (sensor_id, data, read_date_time) values (@sensor_id, @data, SYSDATETIME())
-end
-
-go
-
-exec sp_insert_sensor_with_port 'Test sensor', '134.67.90.286', 34000, 123
-
-select * from sensor_data
-
-go 
-
-create procedure sp_insert_sensor_data(@sensor_id int, @data real, @read_date_time datetime)
-as
-begin
-	insert into sensor_data (sensor_id, data, read_date_time) values (@sensor_id, @data, @read_date_time)
-end
-
-
-exec sp_insert_sensor_data 1, 56, '2021-08-08 12:34:59'
-
- 
+-- Stokta bulunan ürünleri kategorilerine göre gruplayarak en çok kar getiren ürünlerden maksimum karları 20000 den büyük olanların fiyatlarını veren sorgu
+select MAX(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
+where p.stock > 0 group by c.category_id, c.description having MAX(p.stock * (p.price - p.cost)) > 20000
