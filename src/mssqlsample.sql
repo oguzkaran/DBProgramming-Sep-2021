d983804a4acb0106ebdfef152182ffcc3c616b7f
Oğuz Karan <oguzkaran@csystem.org>
2022-01-21 21:07:05 +0300

    Update

--- src/mssqlsample.sql
+++ src/mssqlsample.sql
@@ -22,6 +22,7 @@
 		- lecture_code
 		- grade_id
 
+	Harf notları aşağıdaki gibi olabilir:
 	AA	-> 4.0
 	BA	-> 3.5
 	BB	-> 3.0
@@ -41,46 +42,3 @@
 	- Dersi 3'den fazla kez alan öğrencinin kayıt olduğu derslerin not ortalamasını getiren sorgu
 ----------------------------------------------------------------------------------------------------------------------*/
 
-create database shoppingdb
-
-go
-
-use shoppingdb
-
-go
-
-create table categories (
-	category_id int primary key identity(1, 1),
-	description nvarchar(50) not null
-)
-
-go
-
-insert into categories (description) values ('Electronics'), ('Wear'), ('Auto')
-
-
-go
-
-create table products (
-	product_id int primary key identity(1, 1),
-	category_id int foreign key references categories(category_id),
-	code varchar(100) unique not null,
-	name nvarchar(100) not null,
-	stock int check(stock >= 0) not null,
-	cost money not null,
-	price money not null
-)
-
-go
-
--- Stokta bulunan ürünleri ketegorilerine göre gruplanlayarak kar-zarar durumunu veren sorgu
-select SUM(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
-where p.stock > 0 group by c.category_id, c.description
-
--- Stokta bulunan ürünleri kategorilerine göre gruplayarak en çok kar getiren ürünlerin fiyatlarını veren sorgu
-select MAX(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
-where p.stock > 0 group by c.category_id, c.description having MAX(p.stock * (p.price - p.cost)) > 0
-
--- Stokta bulunan ürünleri kategorilerine göre gruplayarak en çok kar getiren ürünlerden maksimum karları 20000 den büyük olanların fiyatlarını veren sorgu
-select MAX(p.stock * (p.price - p.cost)), c.description from products p inner join categories c on p.category_id=c.category_id
-where p.stock > 0 group by c.category_id, c.description having MAX(p.stock * (p.price - p.cost)) > 20000
