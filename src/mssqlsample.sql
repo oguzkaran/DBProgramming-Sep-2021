1a7e8599cd021b555ec12e15b77858d2020358b4
Oğuz Karan <oguzkaran@csystem.org>
2022-02-25 21:29:05 +0300

    Update

--- src/mssqlsample.sql
+++ src/mssqlsample.sql
@@ -1,46 +1,63 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: students tablosuna yaşı 18'den büyük olan öğrenci insert edilmeye çalışılırsa older_students
-	tabosuna ekleyen insert trigger'ı yazınız.
-	Açıklamalar:
-		- older_students için de aynı trigger yazılmalıdır
-		- Trigger içerisinde hangi tabloya ekleme yapılıyorsa diğer tablodan o citizen_id bilgisinin varlığı kontrol
-		edilmelidir, varsa ekleme yapılmamalıdır
+	Sınıf Çalışması: Aşağıda açıklanan 
+	Tablolar:
+		- devices
+			- id
+			- name
+			- host
+		- ports
+			- id
+			- device_id
+			- port_num
+
+	- Parametresi ile aldığı isim, host ve bir tane port numarasına göre cihazı ve port numarasını ekleyen
+	sp_insert_device_with_port isimli procedure'ü yazınız
+
+	- Procedure numarasının daha önce eklenip eklenmediğini ve port numarasının [1024, 65536] aralığında olduğunu 
+	kontrol edecektir. 
+
+	- Hata durumlarında tüm işlemler geri alınacaktır
+
+	- Procedure transaction safe yapılacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 
 create table students (
-	citizen_id char(11) primary key,
-	name nvarchar(100) not null,
+	student_id int primary key identity(1, 1),
+	name nvarchar(300) not null,
+	citizen_id char(11) unique check(LEN(citizen_id) = 11) not null,
 	birth_date date not null
 )
 
-go
 
-create table older_students (
-	citizen_id char(11) primary key,
-	name nvarchar(100) not null,
-	birth_date date not null
-)
+begin tran
 
-go
+declare @status int = 0
 
-create trigger t_insert_student_instead_of on students
-instead of insert
-as
-begin
-	declare @birth_date date = (select birth_date from inserted)
-	declare @age real = DATEDIFF(DAY, @birth_date, GETDATE()) / 365.
+delete from students where student_id = 8
 
-	if @age > 18
-		insert into older_students (birth_date, citizen_id, name) (select birth_date, citizen_id, name from inserted)
-	else
-		insert into students (birth_date, citizen_id, name) (select birth_date, citizen_id, name from inserted)
-end
+set @status = @@ERROR
+
+if @status <> 0
+	goto END_TRANSACTION
+
+insert into students (name, citizen_id, birth_date) values ('Ayşe', '12345678900', '1978-05-13')
+
+set @status = @@ERROR
+
+if @status <> 0
+	goto END_TRANSACTION
+
+select @@IDENTITY
+
+commit tran
+
+END_TRANSACTION:
+	if @status <> 0
+		rollback tran
+
+-- ...
 
 
-go
+ select * from students
 
-insert into students (name, birth_date, citizen_id) values ('Oğuz Karan', '1976-09-10', '12345678912')
-insert into students (name, birth_date, citizen_id) values ('Ali Veli', '2019-09-10', '12345678913')
 
-select * from older_students
-select * from students
